25c174469d69bc065237a6da40d82b48f339b67d
bkmcco19 <brenna.mcconnell0125@gmail.com>
2022-08-30 14:56:55 -0400

    Update file to include changes

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,8 +2,15 @@
 
 public class HelloWorld {
 
-    public static void main(String [] args){
+    public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("How are you?");
+        //Test comment
+
+        int x = 5;
+        boolean z = true;
+        double pi = 3.14159;
 
     }
 }
+
